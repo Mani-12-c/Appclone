e4468daafdf3e52af5885a7ec9da81f783f084f6
Mani <manikanta3790@gmail.com>
2024-03-12 01:42:21 +0530

    card creation completed

--- cloneapp/src/components/common/Card.js
+++ cloneapp/src/components/common/Card.js
@@ -8,10 +8,21 @@ const Card = (data) => {
     <div className='cardcont'>
         <div className='imgcont'><img src={data.data.url}/></div>
         <div className='datacont' >
+          <div className='dat'>
             <article className='cardarticle'>{data.data.title}</article>
+            <div className='rating'><div className='flex'>{data.data.rating}<i class='material-icons staricn'>star</i></div></div>
+          </div>
+          <div className='dat'>
             <p className='des'>{data.data.des}</p>
+            <p className='price'>{data.data.price}</p>
+          </div>
+          <div className='dat'>
             <p className='loc'>{data.data.loc}</p>
+            <p>{data.data.dist}</p>
+          </div>
+          <div className='dat'>
             <p className='time'>{data.data.time}</p>
+          </div>
         </div>
     </div>
   )
--- cloneapp/src/components/diningout/Diningout.js
+++ cloneapp/src/components/diningout/Diningout.js
@@ -15,53 +15,59 @@ const data = [
         title:"Paradise Biriyani",
         des:"Biriyani,Kebab,North india",
         loc:"Gajuwaka,Vizag",
-        time:"Closes in 4 minutes"
+        time:"Closes in 4 minutes",
+        rating:"4.0",
+        price:"₹850 for two",
+        dist:"1.6km"
     }
 ]
 
 const Diningout = () => {
     return (
-        <section className='maincontainer'>
-            <div className='max-width '>
-                <h2><article className='artcle'>Collections</article></h2>
-                <p>Explore curated lists of top restaurants, cafes, pubs, and bars in Visakhapatnam, based on trends</p>
-                <div className='cardscontainer flex-items'>
-                    {items.map((item, index) => (
-                        <div key={index} className='cardcontainer'>
-                            <div className='imgcontainer'>
-                                <img src={item} className='itemimg' alt={`Image ${index}`} />
+        <section>
+            <section className='maincontainer'>
+                <div className='max-width '>
+                    <h2><article className='artcle'>Collections</article></h2>
+                    <p>Explore curated lists of top restaurants, cafes, pubs, and bars in Visakhapatnam, based on trends</p>
+                    <div className='cardscontainer flex-items'>
+                        {items.map((item, index) => (
+                            <div key={index} className='cardcontainer'>
+                                <div className='imgcontainer'>
+                                    <img src={item} className='itemimg' alt={`Image ${index}`} />
+                                </div>
+                                <div className='contents'>
+                                    <div className='c1'><span>9 Local Favourite places</span></div>
+                                    <div className='c2'><span>9 Places<i class="material-icons">arrow_right</i></span></div>
+                                </div>
                             </div>
-                            <div className='contents'>
-                                <div className='c1'><span>9 Local Favourite places</span></div>
-                                <div className='c2'><span>9 Places<i class="material-icons">arrow_right</i></span></div>
-                            </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
-            </div>
-            <div className='max-width filtermenu'>
-                <div className='cont'><i class="material-icons">tune</i> Filter</div>
-                <div className='cont'><img src='https://b.zmtcdn.com/data/o2_assets/577bf55ff265ae45e11cfe6911d176941687789024.png' className='goldimg'/>Gold</div>
-                <div className='cont'>Rating: 4.0+</div>
-                <div className='cont'>Serves Alcohol</div>
-                <div className='cont'>Outdoor Seating</div>
-                <div className='cont'>Open Now</div>
-            </div>
-            <div className='max-width'>
-                <div className='offerimg'>
-                    <img src="https://b.zmtcdn.com/data/o2_assets/da94405b04f6ae6bf64a4e2a01b1b5c11686563732.png"/>
+            </section>
+                <div className='max-width filtermenu'>
+                    <div className='cont'><i class="material-icons">tune</i> Filter</div>
+                    <div className='cont'><img src='https://b.zmtcdn.com/data/o2_assets/577bf55ff265ae45e11cfe6911d176941687789024.png' className='goldimg'/>Gold</div>
+                    <div className='cont'>Rating: 4.0+</div>
+                    <div className='cont'>Serves Alcohol</div>
+                    <div className='cont'>Outdoor Seating</div>
+                    <div className='cont'>Open Now</div>
                 </div>
-                
-            </div>
+                <div className='max-width'>
+                    <div className='offerimg'>
+                        <img src="https://b.zmtcdn.com/data/o2_assets/da94405b04f6ae6bf64a4e2a01b1b5c11686563732.png"/>
+                    </div>
+                    
+                </div>
 
-            <div className='max-width'>
-                <div>
-                    <article className='artcle'>Trending dining restaurants in Visakhapatnam</article>
+                <div className='max-width'>
+                    <div>
+                        <article className='artcle'>Trending dining restaurants in Visakhapatnam</article>
+                    </div>
+                    <div>
+                        <Card data={data[0]}/>
+                    </div>
                 </div>
-                <div>
-                    <Card data={data[0]}/>
-                </div>
-            </div>
+            
         </section>
     )
 }
